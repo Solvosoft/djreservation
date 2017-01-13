160203bfc6524ee401961506fa42b813cad0c1bd
Mau Rodríguez Morales <maucrodriguez@gmail.com>
2017-01-13 11:54:28 +0000

    Pulling version 0.2.4

--- djreservation/admin.py
+++ djreservation/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Reservation, Observation, Product
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.template.loader import render_to_string
 from django.utils.html import format_html_join
 from django.utils.safestring import mark_safe
-from django.template.loader import render_to_string
+
 from .email import send_reservation_email
+from .models import Reservation, Observation, Product
+from .product_manager import proccess_reservation
 
 # Register your models here.
 
@@ -13,6 +15,21 @@ class ObservationInline(admin.StackedInline):
     model = Observation
     #fields = '__all__'
     extra = 0
+
+
+def different(l1, l2):
+    l1 = list(l1)
+    l2 = list(l2)
+    for x in l1:
+        if x in l2:
+            l1.remove(x)
+            l2.remove(x)
+
+    for x in l2:
+        if x in l1:
+            l1.remove(x)
+            l2.remove(x)
+    return l1 + l2
 
 
 class ReservationAdmin(admin.ModelAdmin):
@@ -24,6 +41,7 @@ class ReservationAdmin(admin.ModelAdmin):
     list_filter = ['status']
     search_fields = ['user__firstname', "user__lastname"]
     inlines = [ObservationInline]
+    date_hierarchy = 'reserved_start_date'
     fieldsets = (
         (None, {
             'fields': (
@@ -33,6 +51,19 @@ class ReservationAdmin(admin.ModelAdmin):
             )
         }),
     )
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly =  admin.ModelAdmin.get_readonly_fields(self, request, obj=obj)
+        if obj and obj.status == obj.RETURNED:
+            readonly= ("user", "status", "updated_datetime",
+                "reserved_start_date", "reserved_end_date",
+                "list_of_products")
+        return readonly
+
+    def get_queryset(self, request):
+        queryset = admin.ModelAdmin.get_queryset(self, request)
+        queryset = queryset.exclude(status=Reservation.BUILDING)
+        return queryset
 
     def list_of_products(self, instance):
 
@@ -44,6 +75,7 @@ class ReservationAdmin(admin.ModelAdmin):
     list_of_products.short_description = "list of products"
 
     def save_model(self, request, obj, form, change):
+        differ_obj = []
         old_status, product_change = -1, False
         if change:
             old_status = obj.__class__.objects.filter(
@@ -54,10 +86,8 @@ class ReservationAdmin(admin.ModelAdmin):
             old_pks = list(map(lambda x: str(x[0]),
                                obj.product_set.all().filter(
                 borrowed=True).values_list("pk")))
-
-            if not (len(product_pks) == len(old_pks) and not any(
-                    set(product_pks) - set(old_pks))):
-
+            differ_obj = different(product_pks, old_pks)
+            if any(differ_obj):
                 obj.product_set.all().exclude(
                     pk__in=product_pks).update(borrowed=False)
 
@@ -65,9 +95,10 @@ class ReservationAdmin(admin.ModelAdmin):
                     pk__in=product_pks).update(borrowed=True)
                 product_change = True
 
-        if product_change or int(old_status) != obj.status:
-            send_reservation_email(obj, request.user)
-
+        change_status = int(old_status) != obj.status
+        if product_change or change_status:
+            #            send_reservation_email(obj, request.user)
+            proccess_reservation(obj, differ_obj, change_status)
         return dev
 
 admin.site.register(Reservation, ReservationAdmin)
--- djreservation/signals.py
+++ djreservation/signals.py
@@ -5,23 +5,13 @@ Free as freedom will be 5/10/2016
 
 @author: luisza
 '''
-
 from __future__ import unicode_literals
 
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from djreservation.models import Reservation
+
 from djreservation.email import send_reservation_email
-
-"""
-@receiver(post_save, sender=Reservation)
-def update_product_related(sender, **kwargs):
-    instance = kwargs['instance']
-    if kwargs['created']:
-        print("TERMINAR AQUI")
-    print(kwargs)
-
-"""
+from djreservation.models import Reservation
 
 
 @receiver(pre_save, sender=Reservation)
@@ -35,18 +25,3 @@ def update_product_related(sender, **kwargs):
         return
 
     send_reservation_email(instance, instance.user)
-
-    # ACCEPTED reservation status
-    if instance.status == str(sender.ACCEPTED):
-        for product in instance.product_set.filter(borrowed=True):
-            ref_obj = product.content_object
-            setattr(ref_obj, product.amount_field,
-                    getattr(ref_obj, product.amount_field) - product.amount)
-            ref_obj.save()
-
-    # RETURNED reservation status
-    elif instance.status == str(sender.RETURNED):
-        for product in instance.product_set.filter():
-            ref_obj = product.content_object
-            setattr(ref_obj, product.amount_field, getattr(ref_obj, product.amount_field) + product.amount)
-            ref_obj.save()
